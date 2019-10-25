38bec43d43225db558714a3bca578d278a5b6315
Utz Nisslmueller <utzn42@univie.ac.at>
2019-10-26 00:38:16 +0200

    Fixed #2, finished connectivity_test. Added some helper classes to help_utils.py along with making connectivity_test output more user friendly.

--- src/connectivity_test.py
+++ src/connectivity_test.py
@@ -1,6 +1,6 @@
 import argparse
 
-from help_utils import print_json
+import help_utils as utils
 from lightning import LightningRpc
 
 parser = argparse.ArgumentParser()
@@ -11,28 +11,32 @@ args = parser.parse_args()
 
 
 def initialize(rpc_path):
-    print("Loading RPC object at: " + rpc_path + "...")
+    print("Loading RPC object at: " + rpc_path + "..." + "\n")
     try:
         temp_rpc_object = LightningRpc(rpc_path)
-        print_json(temp_rpc_object.getinfo())
+        info = utils.InitInfo(temp_rpc_object.getinfo())
+        print("Node ID: " + info.node_id + "\n" + "Block height: " + str(
+            info.block_height) + " (" + info.network + ")" + "\n"
+              + "Version: " + info.version + "\n")
         return temp_rpc_object
     except FileNotFoundError:
-        print("Could not load RPC object at " + rpc_path + ".")
+        print("Could not load RPC object at " + rpc_path + "." + "\n")
 
 
 def print_address_balances(funds):
-    addresses = {}
-    for output in funds["outputs"]:
-        if output["address"] in addresses:
-            addresses[output["address"]] += output["amount_msat"]
-        else:
-            addresses[output["address"]] = output["amount_msat"]
-    print("\n" + "Addresses / Funds:")
-    print(addresses)
+    funds = utils.FundsInfo(funds)
+    print("Addresses / Funds (BTC):")
+    print(funds.address_funds, end="\n \n")
 
 
-# TODO: List channels
+def print_channel_info(peers):
+    channel_info = utils.ChannelInfo(peers)
+    print("Currently connected to " + str(len(peers.keys())) + " peer(s) over " + str(
+        channel_info.count) + " channel(s).")
+    print("Total BTC count: " + str(channel_info.total_msat / 100000000000.0) + "\n")
+
 
 rpc_object = initialize(args.rpc_path)
 print_address_balances(rpc_object.listfunds())
-print("\n" + "Successfully connected to local Lightning daemon!")
+print_channel_info(rpc_object.listpeers())
+print("Successfully connected to local Lightning daemon!")
--- src/help_utils.py
+++ src/help_utils.py
@@ -1,5 +1,36 @@
 import json
 
 
+class InitInfo:
+    def __init__(self, info):
+        self.node_id = info["id"]
+        self.block_height = info["blockheight"]
+        self.network = info["network"]
+        self.version = info["version"]
+
+
+class FundsInfo:
+    def __init__(self, funds):
+        self.address_funds = {}
+        for output in funds["outputs"]:
+            if output["address"] in self.address_funds:
+                self.address_funds[output["address"]] += output["value"] / 100000000.0
+            else:
+                self.address_funds[output["address"]] = output["value"] / 100000000.0
+
+
+class ChannelInfo:
+
+    def __init__(self, peers):
+        channels = []
+        for peer in peers["peers"]:
+            channels += peer["channels"]
+        self.count = len(channels)
+
+        self.total_msat = 0
+        for channel in channels:
+            self.total_msat += channel["msatoshi_total"]
+
+
 def print_json(raw_data):
     print(json.dumps(raw_data, indent=4, sort_keys=True, default=str))
