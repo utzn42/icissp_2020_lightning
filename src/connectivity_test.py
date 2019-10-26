44413f1be9548b520408c461265553120eccd3a9
Utz Nisslmueller <utzn42@univie.ac.at>
2019-10-26 15:04:13 +0200

    Fixed #5 (added documentation)

--- src/connectivity_test.py
+++ src/connectivity_test.py
@@ -1,8 +1,13 @@
+"""A script which connects to a lightning RPC object, and then prints some basic information along with the addresses
+and the corresponding balances as well as a summary of connected peers and channels. """
+
 import argparse
 
-import help_utils as utils
 from lightning import LightningRpc
 
+import help_utils as utils
+
+# Parse command line arguments.
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file",
                     help="file path of the callable RPC object", default="/home/utz/.lightning/lightning-rpc",
@@ -10,7 +15,11 @@ parser.add_argument("-f", "--file",
 args = parser.parse_args()
 
 
-def initialize(rpc_path):
+def initialize(rpc_path: str) -> LightningRpc:
+    """"Takes the path of the RPC file as a parameter and runs getinfo(). If successful, some basic information is
+    printed, and an RPC object is returned for further use. If not, an error is raised, informing the user of an
+    incorrect file path. """
+
     print("Loading RPC object at: " + rpc_path + "..." + "\n")
     try:
         temp_rpc_object = LightningRpc(rpc_path)
@@ -23,19 +32,26 @@ def initialize(rpc_path):
         print("Could not load RPC object at " + rpc_path + "." + "\n")
 
 
-def print_address_balances(funds):
-    funds = utils.FundsInfo(funds)
+def print_address_balances(funds: dict):
+    """Takes in a dictionary returned by LightningRpc.listfunds() and prints addresses and corresponding funds for the
+    current instance. """
+
+    local_funds = utils.FundsInfo(funds)
     print("Addresses / Funds (BTC):")
-    print(funds.address_funds, end="\n \n")
+    print(local_funds.address_funds, end="\n \n")
 
 
-def print_channel_info(peers):
+def print_channel_info(peers: dict):
+    """Takes in a dictionary returned by LightningRpc.listpeers() and displays an executive summary on the amount of
+    connections as well as the amount of BTC within active channels. """
+
     channel_info = utils.ChannelInfo(peers)
     print("Currently connected to " + str(len(peers.keys())) + " peer(s) over " + str(
         channel_info.count) + " channel(s).")
-    print("Total BTC count: " + str(channel_info.total_msat / 100000000000.0) + "\n")
+    print("Total BTC count: " + str(channel_info.total_btc) + "\n")
 
 
+# Main program sequence starts here:
 rpc_object = initialize(args.rpc_path)
 print_address_balances(rpc_object.listfunds())
 print_channel_info(rpc_object.listpeers())
--- src/help_utils.py
+++ src/help_utils.py
@@ -1,8 +1,14 @@
+"""This file contains helper classes and functions which facilitate data handling in other programs and increase
+their readability. """
+
 import json
 
 
 class InitInfo:
-    def __init__(self, info):
+    """Takes in a getinfo() return object and stores the node ID, block height, operating chain and protocol version
+    for further use. """
+
+    def __init__(self, info: dict):
         self.node_id = info["id"]
         self.block_height = info["blockheight"]
         self.network = info["network"]
@@ -10,27 +16,47 @@ class InitInfo:
 
 
 class FundsInfo:
-    def __init__(self, funds):
+    """Takes in a listfunds() return object and keeps a dictionary of found addresses and their corresponding
+    balances. """
+
+    def __init__(self, funds: dict):
         self.address_funds = {}
+
         for output in funds["outputs"]:
+
+            # If the address already exists within the dictionary, append the output value to the existing balance.
             if output["address"] in self.address_funds:
-                self.address_funds[output["address"]] += output["value"] / 100000000.0
+
+                # Divide by 100000000 to convert Millisatoshi to BTC.
+                self.address_funds[output["address"]] += output["value"] / 100000000
+
+            # If the address does not exist, create a new entry for the address and store the corresponding balance.
             else:
-                self.address_funds[output["address"]] = output["value"] / 100000000.0
+
+                # Divide by 100000000 to convert Millisatoshi to BTC.
+                self.address_funds[output["address"]] = output["value"] / 100000000
 
 
 class ChannelInfo:
+    """Takes in a listpeers() return objects and counts the amount of active peers and channels. It then calculates
+    the total amount of BTC locked within these channels. """
 
-    def __init__(self, peers):
+    def __init__(self, peers: dict):
+
+        # Count peers and channels.
         channels = []
         for peer in peers["peers"]:
             channels += peer["channels"]
         self.count = len(channels)
 
-        self.total_msat = 0
+        # Calculate BTC amount within channels.
+        self.total_btc = 0
         for channel in channels:
-            self.total_msat += channel["msatoshi_total"]
+            # Divide by 100000000 to convert Millisatoshi to BTC.
+            self.total_btc += channel["msatoshi_total"] / 100000000
 
 
 def print_json(raw_data):
+    """Prints a JSON object, and parses unknown data types into strings."""
+
     print(json.dumps(raw_data, indent=4, sort_keys=True, default=str))
