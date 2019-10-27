988e078c7802b038b13be548797ec241f2fa21d4
Utz Nisslmueller <utzn42@univie.ac.at>
2019-10-28 00:19:28 +0100

    Worked on #6, specifically on implementing the TCP handshake before sending a ping packet.

--- src/ln_mapper.py
+++ src/ln_mapper.py
@@ -3,5 +3,12 @@ from scapy.all import *
 from scapy.layers.inet import IP, TCP
 
 # TODO: Set up full TCP handshake
-packets = IP(src="192.168.0.59", dst="203.132.95.10") / TCP(dport=9735, flags="S") / PingPong()
-print(sr1(packets))
+source_port = 9735
+dest_port = 9735
+ip_packet = IP(src="192.168.0.59", dst="88.99.209.230")
+tcp_syn = TCP(sport=source_port, dport=dest_port, flags="S", seq=1000)
+ping = PingPong()
+synack = sr1(ip_packet / tcp_syn)
+ack = TCP(sport=source_port, dport=dest_port, flags='A', seq=synack.ack + 1, ack=synack.seq + 1)
+send(ip_packet / ack)
+sr1(ip_packet/TCP/ping)
